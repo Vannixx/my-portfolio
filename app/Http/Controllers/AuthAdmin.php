3b306d117c699a9929b5d7c5786e42e2bcc75c70
Fred <72584933+Vannixx@users.noreply.github.com>
2024-10-06 21:10:34 +0800

    *

--- app/Http/Controllers/AuthAdmin.php
+++ app/Http/Controllers/AuthAdmin.php
@@ -11,54 +11,57 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthAdmin extends Controller
 {
-    function dashboard(){
+    function dashboard()
+    {
         return view('admin.dashboard');
     }
     //to check if nag-log in ang user
-    function login(){
-        if(Auth::check()){
+    function login()
+    {
+        if (Auth::check()) {
             return redirect()->intended(route('userprofile'))->with("success");
         }
         return view('admin.Login');
     }
     //Register
-    function registerPost(Request $request){
+    function registerPost(Request $request)
+    {
         $request->validate([
             'name' => 'required',
-            'username'=>'required',
-            'password'=>'required',
+            'username' => 'required',
+            'password' => 'required',
         ]);
 
         $data['name'] = $request->name;
         $data['username'] = $request->username;
         $data['password'] = $request->password;
         $user = User::create($data);
-        if(!$user){
-            return redirect(route('login'))->with("error","Registration Failed");
+        if (!$user) {
+            return redirect(route('login'))->with("error", "Registration Failed");
         }
-        return redirect(route('login'))->with("success","Registered Successfully!");
-
+        return redirect(route('login'))->with("success", "Registered Successfully!");
     }
 
     //Login
-    function loginPost(Request $request){
+    function loginPost(Request $request)
+    {
         $request->validate([
-            'username'=>'required',
-            'password'=>'required',
+            'username' => 'required',
+            'password' => 'required',
         ]);
         $credentials = $request->only('username', 'password');
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             $user = Auth::user();
-            return redirect(route('userprofile'))->with("success","Loged in Successfully");
+            return redirect(route('userprofile'))->with("success", "Loged in Successfully");
         }
         return redirect(route('login'));
     }
 
     //logout
-    function logout(){
+    function logout()
+    {
         Session::flush();
         Auth::logout();
         return redirect()->route('login');
     }
-
 }
